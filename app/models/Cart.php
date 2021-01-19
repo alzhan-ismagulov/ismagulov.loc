d97b9994f6580984c05eb241ad4ca886ea91d769
alzhan-ismagulov <aismagulov@gmail.com>
2021-01-19 01:30:07 +0100

    Корзина Часть 3 окончена

---
+++ app/models/Cart.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use ismagulova\App;
+
+class Cart extends AppModel
+{
+    public function addToCart($course, $qty = 1)
+    {
+        //Для выбора валют
+//        if (!isset($_SESSION['cart.currency'])){
+//            $_SESSION['cart.currency'] = App::$app->getProperty('currency');
+//        }
+        $id = $course->id;
+        $title = $course->name;
+        $price = $course->price;
+
+        if (isset($_SESSION['cart'][$id])){
+            $_SESSION['cart'][$id] = [
+              'qty' => $qty,
+              'title' => $title,
+                //Для выбора валют
+//              'price' => $price * $_SESSION['cart.currency']['value'],
+              'price' => $price,
+            ];
+        }
+        if (isset($_SESSION['cart'][$id])){
+            $_SESSION['cart'][$id]['qty'] += $qty;
+        } else {
+            $_SESSION['cart'][$id] = [
+                'qty' => $qty,
+                'title' => $title,
+                'price' => $price
+            ];
+        }
+        $_SESSION['cart.qty'] = isset($_SESSION['cart.qty']) ? $_SESSION['cart.qty'] + $qty : $qty;
+        if (!isset($_SESSION['cart.sum'])) {
+            $_SESSION['cart.sum'] = $price;
+        } else {
+            $_SESSION['cart.sum'] += $price;
+        }
+    }
+}
