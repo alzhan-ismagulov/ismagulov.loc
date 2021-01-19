cfe69254b30976853bfca4c01a2aacf86b9bf5cd
alzhan-ismagulov <aismagulov@gmail.com>
2021-01-19 01:31:59 +0100

    Корзина Часть 3 окончена

---
+++ public/js/cart.js
@@ -0,0 +1,26 @@
+$('body').on('click', '.rates__btn', function(e){
+    e.preventDefault();
+    var id = $(this).data('id'),
+        qty = 1; //проба
+    $.ajax({
+        url: 'cart/add',//Куда передаются данные
+        // data: {id: id},//Передача данных
+        data: {id: id, qty: qty},//Передача данных
+        type: 'GET',//Каким типом идёт передача
+        success: function (res) {
+            showCart(res);
+        },
+        error: function () {
+            alert('Ошибка. Попробуйте снова');
+        }
+    })
+});
+function showCart(cart) {
+    if ($.trim(cart) == '<h3>Корзина пуста</h3>'){
+        $('#cart .modal-footer a, #cart .modal-footer .btn-danger').css('display', 'none');
+    } else {
+        $('#cart .modal-footer a, #cart .modal-footer .btn-danger').css('display', 'inline-block');
+    }
+    $('#cart .modal-body').html(cart);
+    $('#cart').modal();
+}
